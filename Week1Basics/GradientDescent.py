c52c767a7da6e5cf7b13d28af6d22ca06001aaa0
Universe <daibbar.mohamed@gmail.com>
2025-06-20 19:57:07 +0100

    initilize the project

---
+++ Week1Basics/GradientDescent.py
@@ -0,0 +1,21 @@
+## reel polynomiale functions ax2 +bx +c
+
+a = float(input("write a coef: "))
+b = float(input("write b coef: "))
+c = float(input("write c coef: "))
+
+x =0
+g = (2*a*x +b )*(0.1)
+for i in range(5):
+    x = x - g
+
+print(x)
+
+
+
+
+
+
+
+
+
