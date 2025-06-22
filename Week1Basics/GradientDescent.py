5b7aeb6dbf815d40e1e15db845d50dc3c8199a00
Universe <daibbar.mohamed@gmail.com>
2025-06-22 03:28:21 +0100

    print 20 values of x and f(x)

--- Week1Basics/GradientDescent.py
+++ Week1Basics/GradientDescent.py
@@ -1,17 +1,35 @@
 ## reel polynomiale functions ax2 +bx +c
+import numpy as np
+import matplotlib.pyplot as plt
 
 a = float(input("write a coef: "))
 b = float(input("write b coef: "))
 c = float(input("write c coef: "))
 
-x =0
-g = (2*a*x +b )*(0.1)
-for i in range(5):
-    x = x - g
 
-print(x)
+learningRate = 0.1
 
 
+xValues = []
+xImages = []
+
+x = 0
+for i in range(20):
+    gradientF = (2 * a * x + b)  
+    x = x - learningRate * gradientF
+    fx = a * x * x + b * x + c
+    xValues.append(round(x,4))
+    xImages.append(round(fx,4))
+
+
+
+
+for i in range(19):
+  print(f"x{i} = {xValues[i]} --> fx{i} = {xImages[i]}")
+
+
+
+    
 
 
 
